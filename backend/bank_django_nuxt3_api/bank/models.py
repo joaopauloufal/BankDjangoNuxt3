03629ac8ec2553c44a213bd6a00bc418e5951dc2
joao.silva <joao.silva@arapiraca.al.gov.br>
2022-04-19 20:04:59 -0300

    Agency model

--- backend/bank_django_nuxt3_api/bank/models.py
+++ backend/bank_django_nuxt3_api/bank/models.py
@@ -11,3 +11,17 @@ class Bank(models.Model):
 
     def __str__(self):
         return f'{self.bank_code} - {self.name}'
+
+
+class Agency(models.Model):
+    agency_code = models.CharField(verbose_name='Agency Code', max_length=20)
+    name = models.CharField(verbose_name='Name', max_length=120)
+    bank = models.ForeignKey(Bank, verbose_name='Bank', on_delete=models.PROTECT, related_name='agencies')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Agency'
+        unique_together = [['agency_code', 'bank']]
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
