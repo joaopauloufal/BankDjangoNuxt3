eb564970be668dca013674f94cd99542f59e7c95
joao.silva <joao.silva@arapiraca.al.gov.br>
2022-05-05 19:15:54 -0300

    Add oruga

--- frontend/bank_django_nuxt3_frontend/nuxt.config.ts
+++ frontend/bank_django_nuxt3_frontend/nuxt.config.ts
@@ -2,5 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false
+  ssr: false,
+  css: [
+    // SCSS file in the project
+    '@oruga-ui/oruga-next/src/scss/oruga-full-vars.scss',
+  ],
+  build: {
+    transpile: [/oruga/]
+  },
 })
---
+++ frontend/bank_django_nuxt3_frontend/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <o-button variant="primary">Primary</o-button>
+    <o-button variant="success">Success</o-button>
+    <o-button variant="danger">Danger</o-button>
+    <o-button variant="warning">Warning</o-button>
+    <o-button variant="info">Info</o-button>
+  </div>
+</template>
